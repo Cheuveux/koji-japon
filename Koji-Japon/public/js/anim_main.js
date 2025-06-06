e9d640556d83ee9537a9385f501ea7b2ca33682c
Cheveux <gaspard.szuszkin@gmail.com>
2025-06-06 12:20:52 +0200

    Supprime le sous-module Koji-Japon

---
+++ Koji-Japon/public/js/anim_main.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() {
+    const mains = document.querySelectorAll('main');
+    mains.forEach(main => {
+        main.classList.add('loaded');
+    });
+});
+
